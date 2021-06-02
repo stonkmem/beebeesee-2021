4bd87edddaf7f28b98ba34fabee7104d26f000cf
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-02 21:21:41 +0800

    Set-up data for overfitting

---
+++ randomizeImages.py
@@ -0,0 +1,23 @@
+from glob import glob
+from random import shuffle
+import os
+
+emotions = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
+n = 436
+
+
+for emotion in emotions:
+    files = glob(f"train/{emotion}/*.jpg")
+    shuffle(files)
+    os.chdir("data")
+    os.mkdir(emotion)
+    os.chdir("..")
+
+    for filename in files[:n]:
+        filename = filename.replace("\\", "/")
+        with open(filename, "rb") as readfile:
+            data = readfile.read()
+
+        new_filename = f"data/{emotion}/" + filename.split("/")[-1]
+        with open(new_filename, "wb+") as writefile:
+            writefile.write(data)
