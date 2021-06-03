5aadb0682e043cb645edf0540d10af30b14ad186
RenoirTan <renoirtan2005@gmail.com>
2021-06-03 10:36:03 +0800

    main: Atttempt to fix shit

--- beebeesee/site.py
+++ beebeesee/site.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-from .info import PROJECT_NAME, VIEWS_DIR
+import json
+from flask import Blueprint, render_template, make_response
+from .info import PROJECT_NAME, STATIC_DIR, VIEWS_DIR
 
 site = Blueprint("site", PROJECT_NAME, template_folder=VIEWS_DIR)
 
@@ -7,3 +8,10 @@ site = Blueprint("site", PROJECT_NAME, template_folder=VIEWS_DIR)
 @site.route("/", methods=["GET"])
 def root():
     return render_template("root.html")
+
+
+@site.route("/_model/model", methods=["GET"])
+def _model_model():
+    MODEL_PATH = STATIC_DIR / "model" / "model.json"
+    model = json.load(MODEL_PATH.open("r"))
+    return model
