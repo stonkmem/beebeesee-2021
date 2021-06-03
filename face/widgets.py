1f4ced6693d5620e5d11bd869f1b5b882624ffff
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-03 17:52:26 +0800

    Update widgets.py

--- face/widgets.py
+++ face/widgets.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import numpy as np
 import cv2
 from PIL import ImageTk, Image
 from gooey import Label, Frame, Button, Tk
@@ -9,6 +10,28 @@ emotion_model = create_prototype_emotion_model()
 emotion_model.load_weights('../models/trained_model.h5')
 
 cv2.ocl.setUseOpenCL(False)
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+
+def modifyImg(frame_edit, frame):
+    bounding_box = cv2.CascadeClassifier('../models/haarcascades/haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces:
+        cv2.rectangle(frame_edit, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        emotion_prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(emotion_prediction))
+        cv2.putText(
+            frame_edit, emotion_dict[maxindex],
+            (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+            (255, 255, 255), 2, cv2.LINE_AA
+        )
+    return frame_edit
+
+
 
 class SentimentalWebcam(Frame):
     def __init__(self, master=None):
@@ -17,6 +40,9 @@ class SentimentalWebcam(Frame):
         self.root = Frame(master, bg="white")
 
         self.cap = cv2.VideoCapture(0)
+        flag, frame = self.cap.read()
+        frame = cv2.resize(frame, (600, 500))
+        self.img = cv2.flip(frame, 1)
 
         Label(
             self.root,
@@ -37,8 +63,8 @@ class SentimentalWebcam(Frame):
             command=self.toggle_show,
         ).pack(side=tk.BOTTOM)
 
-        self.boolean = False
-
+        self.boolean = True
+        self.dt = 0
         self.playImg()
 
         if not master:
@@ -48,35 +74,29 @@ class SentimentalWebcam(Frame):
     def playImg(self):
         flag, frame = self.cap.read()
         frame = cv2.resize(frame, (600, 500))
-
         frame = cv2.flip(frame, 1)
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
 
-        self.img = cv2image
+        if self.dt == 500:
+            self.img, frame = frame, modifyImg(frame, self.img)
+            self.dt = 0
 
-        img = Image.fromarray(cv2image)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        frame = cv2.resize(frame, (800, 600))
+
+        img = Image.fromarray(frame)
         imgtk = ImageTk.PhotoImage(image=img)
         self.lmain.imgtk = imgtk
         self.lmain.configure(image=imgtk)
-
+        if self.boolean:
+            self.dt += 10
+            self.lmain.after(10, self.playImg)
 
     def pack(self, *args, **kwargs):
         self.root.pack(*args, **kwargs)
 
     def toggle_show(self):
         self.boolean = not self.boolean
-        self.show()
-
-    def show(self):
-        _, frame = self.cap.read()
-        frame = cv2.flip(frame, 1)
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        img = Image.fromarray(cv2image)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.lmain.imgtk = imgtk
-        self.lmain.configure(image=imgtk)
-        if self.boolean:
-            self.lmain.after(10, self.show)
+        self.playImg()
 
 
 if __name__ == "__main__":
