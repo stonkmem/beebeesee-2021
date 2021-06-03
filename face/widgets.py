88ae5a73cd6f19e01e29eaaa3b5f2281cbffc94c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-03 17:07:39 +0800

    Update Tkinter FACE

--- face/widgets.py
+++ face/widgets.py
@@ -1,52 +1,64 @@
 import tkinter as tk
-from tkinter import ttk
 import cv2
 from PIL import ImageTk, Image
+from gooey import Label, Frame, Button, Tk
 
+from util.ml import create_prototype_emotion_model
 
-class SentimentalWebcam(ttk.Frame):
+emotion_model = create_prototype_emotion_model()
+emotion_model.load_weights('../models/trained_model.h5')
+
+cv2.ocl.setUseOpenCL(False)
+
+class SentimentalWebcam(Frame):
     def __init__(self, master=None):
-        self.master = tk.Tk() if master else master
-        ttk.Frame.__init__(self, self.master, bg="white")
-        self.root = ttk.Frame(master, bg="white")
-        width, height = 800, 600
+        self.master = Tk() if master else master
+        Frame.__init__(self, self.master, bg="white")
+        self.root = Frame(master, bg="white")
+
         self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        label = ttk.Label(
+
+        Label(
             self.root,
             text="Webcam Footage",
             fg="red",
             font=("Trebuchet MS", 15),
             bg="white",
-        )
-        label.pack(side=tk.TOP)
-        self.lmain = ttk.Label(self.root)
+        ).pack(side=tk.TOP)
+
+        self.lmain = Label(self.root, padx=50, bd=10)
         self.lmain.pack(fill=tk.BOTH, expand=1, side=tk.TOP)
-        ttk.Button(
+
+        Button(
             self.root,
             text="Play",
             bg="#19A7A7",
             fg="white",
             command=self.toggle_show,
         ).pack(side=tk.BOTTOM)
+
         self.boolean = False
 
-        _, frame = self.cap.read()
+        self.playImg()
+
+        if not master:
+            self.pack(fill=tk.BOTH, expand=1)
+            self.master.mainloop()
+
+    def playImg(self):
+        flag, frame = self.cap.read()
+        frame = cv2.resize(frame, (600, 500))
+
         frame = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
 
-        self.img = cs2image
-
+        self.img = cv2image
 
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image=img)
         self.lmain.imgtk = imgtk
         self.lmain.configure(image=imgtk)
 
-        if master:
-            self.pack(fill=BOTH, expand=1)
-            self.master.mainloop()
 
     def pack(self, *args, **kwargs):
         self.root.pack(*args, **kwargs)
@@ -65,6 +77,7 @@ class SentimentalWebcam(ttk.Frame):
         self.lmain.configure(image=imgtk)
         if self.boolean:
             self.lmain.after(10, self.show)
+
 
 if __name__ == "__main__":
     SentimentalWebcam()
