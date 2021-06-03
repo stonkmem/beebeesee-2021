003bd2d114765605358be79caedd7cdd8ff306ef
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-03 09:57:41 +0800

    Convert the keras model to JS

--- convertKeras2JS.py
+++ convertKeras2JS.py
@@ -1,8 +1,23 @@
 import tensorflowjs as tfjs
-import tensorflow.keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 
-# Load the model
-model = tensorflow.keras.models.load_model(
-    "models/trained_model.h5"
-)  # keras_model.h5
-tfjs.converters.save_keras_model(model, "models/js")
+emotion_model = Sequential()
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+emotion_model.load_weights('models/trained_model.h5')
+
+tfjs.converters.save_keras_model(emotion_model, "models/model")
