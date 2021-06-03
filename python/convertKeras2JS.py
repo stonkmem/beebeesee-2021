f139724042ba0883139d466a14277189e1f3fc0e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-03 17:06:55 +0800

    Update convertKeras2JS.py

--- python/convertKeras2JS.py
+++ python/convertKeras2JS.py
@@ -4,4 +4,4 @@ from util.ml import create_prototype_emotion_model
 emotion_model = create_prototype_emotion_model()
 emotion_model.load_weights('models/trained_model.h5')
 
-tfjs.converters.save_keras_model(emotion_model, "models/model")
+tfjs.converters.save_keras_model(emotion_model, "../models/model")
