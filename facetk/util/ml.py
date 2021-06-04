6679c7400516276dca3324525f174712fe8d50a7
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-04 17:12:03 +0800

    Add facetk

---
+++ facetk/util/ml.py
@@ -0,0 +1,63 @@
+import tensorflow.keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def testModel(path_to_model, path_to_image):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+
+    # Load the model
+    model = tensorflow.keras.models.load_model(
+        path_to_model
+    )  # keras_model.h5
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open(path_to_image)
+
+    # resize the image to a 224x224 with the same strategy as in TM2:
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # display the resized image
+    image.show()
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    print(prediction)
+
+def create_prototype_emotion_model():
+    emotion_model = Sequential()
+    emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+    emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Dropout(0.25))
+    emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+    emotion_model.add(Dropout(0.25))
+    emotion_model.add(Flatten())
+    emotion_model.add(Dense(1024, activation='relu'))
+    emotion_model.add(Dropout(0.5))
+    emotion_model.add(Dense(7, activation='softmax'))
+    return emotion_model
